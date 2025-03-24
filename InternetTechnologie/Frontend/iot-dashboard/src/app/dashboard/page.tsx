7d213007b72c73716e9f61e3f1c26cace3e1fbab
Richard <73758505+NotDutchy@users.noreply.github.com>
2025-03-24 01:38:57 +0100

    Added names to card and value types

--- InternetTechnologie/Frontend/iot-dashboard/src/app/dashboard/page.tsx
+++ InternetTechnologie/Frontend/iot-dashboard/src/app/dashboard/page.tsx
@@ -2,6 +2,9 @@ import { FaDroplet } from "react-icons/fa6";
 import { CiTempHigh } from "react-icons/ci";
 import { MdCo2 } from "react-icons/md";
 
+let Temperature: number = 20;
+let Humidity: number = 40;
+
 export default function Dashboard() {
     return <div className="flex justify-center items-center bg-gray-100 p-4">
         <div className="card w-80 bg-white shadow-xl p-6 rounded-lg">
@@ -10,7 +13,12 @@ export default function Dashboard() {
                     <FaDroplet size={24}/>
                 </div>
                 <div className="card-body text-black">
-                    Here comes the Humidity value!
+                    <div className="text-2xl">
+                        Humidity
+                    </div>
+                    <div className="text-2xl">
+                        {Humidity}%
+                    </div>
                 </div>
             </div>
         </div>
@@ -20,7 +28,12 @@ export default function Dashboard() {
                     <CiTempHigh size={24}/>
                 </div>
                 <div className="card-body text-black">
-                    Here comes the temperature value!
+                    <div className="text-2xl">
+                        Temperature
+                    </div>
+                    <div className="text-2xl">
+                        {Temperature} &deg;C
+                    </div>
                 </div>
             </div>
         </div>
